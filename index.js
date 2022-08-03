d5c1d9d9d662581da6bea6470d719b9bf5ff4fdb
Shoichi Nakamura <41470674+shoichi1031da@users.noreply.github.com>
2022-08-04 03:50:40 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const https = require("https");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const TOKEN = process.env.LINR_ACCESS_TOKEN;
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get("/", (req,res) => {
+    res.sendStatus(200);
+});
+
+app.post("/webhook", (req,res) => {
+    res.send("HTTP POST request sent to the webhook URL!");
+    // ユーザーがボットにメッセージを送った場合、返信メッセージを送る
+    if (req.body.events[0].type === "message") {
+        // 文字列化したメッセージデータ
+        const dataString = JSON.stringify({
+            replyToken: req.body.events[0].replyToken,
+            messages: [
+                {
+                "type": "text",
+                "text": "Hello, user"
+                },
+                {
+                "type": "text",
+                "text": "May I help you?"
+                }
+            ]
+        })
+
+        // リクエストヘッダー
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + TOKEN
+
+        }
+
+        // リクエストに渡すオプション
+        const webhookOptions = {
+            "hostname": "api.line.me",
+            "path": "/v2/bot/message/reply",
+            "method": "POST",
+            "headers": headers,
+            "body": dataString
+        }   
+
+        // リクエストの定義
+        const request = https.request(webhookOptions, (res) => {
+            res.on("data", (d) => {
+            process.stdout.write(d)
+            })
+        })
+    
+        // エラーをハンドル
+        request.on("error", (err) => {
+            console.error(err)
+        })
+    
+        // データを送信
+        request.write(dataString)
+        request.end()
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+});
