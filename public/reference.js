50d5c3bf58605911a0ed238dd0cdd2a7f118b00b
Shoichi Nakamura <41470674+shoichi1031da@users.noreply.github.com>
2022-08-06 06:31:25 +0900

    aplay to get method from ichigojam

--- public/reference.js
+++ public/reference.js
@@ -27,14 +27,10 @@ const checkCommand = (data,msg) => {
       sendMsg.aname = "";
     }
     sendMsg.format = "■書式：" + sendMsg.format + "\n"; 
-    sendMsg.example = "■例：" + sendMsg.example + "\n"; 
-    if(sendMsg.document){
-      sendMsg.description = "■説明：" + sendMsg.description + "\n"; 
-      sendMsg.document = "■資料：" + sendMsg.document; 
-    }else{
-      sendMsg.description = "■説明：" + sendMsg.description; 
-      sendMsg.document = ""; 
-    }
+    sendMsg.example = "■例　：" + sendMsg.example + "\n"; 
+    sendMsg.description = "■説明：" + sendMsg.description + "\n"; 
+    sendMsg.document = "■資料：" + sendMsg.document; 
+   
     const sendData = {"result":result,"commandInfo":sendMsg};
     
   resolve(sendData);
